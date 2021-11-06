108a8d46162c0ae255fa85f5351fe7469b739314
Ryo Asai <fu.orangina0516@iCloud.com>
2021-11-06 22:30:30 +0900

    usersテーブル作成

--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"todo_app/config"
+	// "log"
+	// "todo_app/config"
+	"todo_app/models"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
 
-	log.Println("test")
+	// log.Println("test")
+
+	fmt.Println(models.Db)
 }
---
+++ models/base.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"todo_app/config"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var Db *sql.DB
+
+var err error
+
+const (
+	tableNameUser = "users"
+)
+
+func init() {
+	Db, err := sql.Open(config.Config.SQLDriver, config.Config.DbName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	cmdU := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		uuid STRING NOT NULL UNIQUE,
+		name STRING,
+		email STRING,
+		password STRING,
+		created_at DATETIME)`, tableNameUser)
+
+	Db.Exec(cmdU)
+}
